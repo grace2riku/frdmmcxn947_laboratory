3b4ef87f5134cdb434b993cb05a93754495fa9ce
Koji Abe <gracehime@gmail.com>
2025-10-08 12:52:10 +0900

    RGB LEDの点灯コードを追加し動作確認した

--- frdmmcxn947/hello_world_cm33_core0/source/hello_world.c
+++ frdmmcxn947/hello_world_cm33_core0/source/hello_world.c
@@ -11,6 +11,9 @@
 #include "board.h"
 #include "app.h"
 
+#include "fsl_gpio.h"
+#include "pin_mux.h"
+
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -42,5 +45,19 @@ int main(void)
     {
         ch = GETCHAR();
         PUTCHAR(ch);
+
+        if ('r' == ch) {
+			GPIO_PinWrite(BOARD_INITPINS_LED_RED_GPIO, BOARD_INITPINS_LED_RED_GPIO_PIN, 0);
+			GPIO_PinWrite(BOARD_INITPINS_LED_GREEN_GPIO, BOARD_INITPINS_LED_GREEN_GPIO_PIN, 1);
+			GPIO_PinWrite(BOARD_INITPINS_LED_BLUE_GPIO, BOARD_INITPINS_LED_BLUE_GPIO_PIN, 1);
+        } else if ('g' == ch) {
+			GPIO_PinWrite(BOARD_INITPINS_LED_RED_GPIO, BOARD_INITPINS_LED_RED_GPIO_PIN, 1);
+			GPIO_PinWrite(BOARD_INITPINS_LED_GREEN_GPIO, BOARD_INITPINS_LED_GREEN_GPIO_PIN, 0);
+			GPIO_PinWrite(BOARD_INITPINS_LED_BLUE_GPIO, BOARD_INITPINS_LED_BLUE_GPIO_PIN, 1);
+        } else if ('b' == ch) {
+			GPIO_PinWrite(BOARD_INITPINS_LED_RED_GPIO, BOARD_INITPINS_LED_RED_GPIO_PIN, 1);
+			GPIO_PinWrite(BOARD_INITPINS_LED_GREEN_GPIO, BOARD_INITPINS_LED_GREEN_GPIO_PIN, 1);
+			GPIO_PinWrite(BOARD_INITPINS_LED_BLUE_GPIO, BOARD_INITPINS_LED_BLUE_GPIO_PIN, 0);
+        }
     }
 }
